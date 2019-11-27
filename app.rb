7f71032c59b4ff68be2b3959ce40886e2ae5df34
James Scalise <jamescscalise@gmail.com>
2019-11-27 21:26:15 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -44,6 +44,8 @@ class App < Sinatra::Base
     @operation = params[:operation]
     @number1 = params[:number1].to_i
     @number2 = params[:number2].to_i
+    if @operation = "add" do
+    end
   end
   
 end
