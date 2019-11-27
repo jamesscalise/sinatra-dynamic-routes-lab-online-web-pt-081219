05fd2db049fe2726cb88c423852fe3fe11ab8bb3
James Scalise <jamescscalise@gmail.com>
2019-11-27 21:28:09 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -46,14 +46,11 @@ class App < Sinatra::Base
     @number2 = params[:number2].to_i
     if @operation = "add"
       (@number1 + @number2).to_s
-    end
-    if @operation = "subtract"
+    elsif @operation = "subtract"
       (@number1 - @number2).to_s
-    end
-    if @operation = "multiply"
+    elsif @operation = "multiply"
       (@number1 * @number2).to_s
-    end
-    if @operation = "divide"
+    elsif @operation = "divide"
       (@number1 / @number2).to_s
     end
   end
